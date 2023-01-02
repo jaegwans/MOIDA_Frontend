86a6c7fa93c263cbb6f1d7b78fca589d603fc240
jaegwans <skhu201811140@gmail.com>
2023-01-02 21:05:01 +0900

    feat: 전역 스타일링

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from 'next/app';
+import Global from '../../styles/styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Global />
+            <Component {...pageProps} />
+        </>
+    );
 }
