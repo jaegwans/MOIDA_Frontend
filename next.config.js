9cabdc6340dba49b52b9f322600bea6843eb87b4
zinu <doglife222@gmail.com>
2023-01-04 01:28:47 +0900

    회원가입 구현

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    compiler: { styledComponents: true },
+    compiler: {
+        styledComponents: true,
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: 'http://moida-skhu.duckdns.org/:path*',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
---
+++ src/libs/api.ts
@@ -0,0 +1,6 @@
+// import axios from "axios";
+
+// export const instance = axios.create({
+//   baseURL: "http://moida-skhu.duckdns.org",
+//   headers: { "Content-Type": "application/json" },
+// });
