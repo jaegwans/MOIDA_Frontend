a7837b1ff0959a5bcb171d6e67c5f4dc11a46ef7
jaegwans <skhu201811140@gmail.com>
2023-01-06 23:46:14 +0900

    fix: 동적 라우팅 가능한 프록시로 수정

--- next.config.js
+++ next.config.js
@@ -1,27 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true,
     reactStrictMode: true,
     compiler: {
         styledComponents: true,
     },
     async rewrites() {
-        return [
-            {
-                source: '/:path*',
-                destination: 'http://moida-skhu.duckdns.org/:path*',
-            },
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'header',
-                        key: 'Authorization',
-                        value: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MDEiLCJhdXRoIjoiUk9MRV9VU0VSIiwiZXhwIjoxNjczMDczNDgzfQ.6-KDa7uMjByDZP7643ri6SmqBki80cqVdJziei3DbaU',
-                    }
-                ],
-                destination: 'http://moida-skhu.duckdns.org/:path*',
-            },
-        ];
+        return {
+            fallback: [
+                {
+                    source: '/:path*',
+                    destination: 'http://moida-skhu.duckdns.org/:path*',
+                },
+            ],
+        };
     },
 };
 
