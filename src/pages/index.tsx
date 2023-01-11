97272649d2f4c9454ca64935d10848cb1b2bc4e1
jaegwans <skhu201811140@gmail.com>
2023-01-11 14:06:17 +0900

    fix: index.tsx 오타 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,18 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { json } from 'stream/consumers';
 import useToken from '../hooks/useToken';
-import useUser from '../libs/useUser';
 import { useRouter } from 'next/router';
 
 type account = string | null;
 
 export default function Home() {
     const [aToken, setAToken] = useState<account>('');
-    const { user } = useUser();
+
     const router = useRouter();
-    console.log(user);
     useEffect(() => {
         setAToken(localStorage.getItem('accessToken'));
         console.log(aToken);
-        if (localStorage.getItem('accesss')) {
+        if (localStorage.getItem('accessToken') === null) {
             console.log(localStorage.getItem('accesss'));
             router.push('/signIn');
         } else {
