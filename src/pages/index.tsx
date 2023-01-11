8f056ba9ba968fa5f0da1b7b3ff244c687669f8d
jaegwans <skhu201811140@gmail.com>
2023-01-11 13:53:54 +0900

    fix: / 페이지 리다이렉션

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,32 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { json } from 'stream/consumers';
 import useToken from '../hooks/useToken';
+import useUser from '../libs/useUser';
+import { useRouter } from 'next/router';
 
 type account = string | null;
 
 export default function Home() {
     const [aToken, setAToken] = useState<account>('');
+    const { user } = useUser();
+    const router = useRouter();
+    console.log(user);
     useEffect(() => {
         setAToken(localStorage.getItem('accessToken'));
+        console.log(aToken);
+        if (localStorage.getItem('accesss')) {
+            console.log(localStorage.getItem('accesss'));
+            router.push('/signIn');
+        } else {
+            router.push('/post/list');
+        }
     }, []);
 
     console.log(aToken + '-Token');
 
     return (
         <>
-            <h2>moida</h2>
-            <p>{aToken}</p>
+            <h2>...Loading</h2>
         </>
     );
 }
