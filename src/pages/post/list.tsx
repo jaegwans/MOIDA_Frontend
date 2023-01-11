ab8454207fce430e547f6ad592cc4546afcc3261
jaegwans <skhu201811140@gmail.com>
2023-01-11 14:11:19 +0900

    test: test

--- src/pages/post/list.tsx
+++ src/pages/post/list.tsx
@@ -1,17 +1,15 @@
-import { useRouter } from "next/router";
-
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const List = () => {
-
     const router = useRouter();
 
-    return(
+    return (
         <div>
             <h1>post list</h1>
-
-            
+            <Link href="/post/detail/1">첫 게시글 조회 test click</Link>
         </div>
-    )
+    );
 };
 
 export default List;
