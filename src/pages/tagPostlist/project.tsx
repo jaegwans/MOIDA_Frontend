61c8ee49f3b68873d29cde3dac7b60a1b8c08d60
jaegwans <skhu201811140@gmail.com>
2023-01-13 18:19:59 +0900

    l3

--- src/pages/tagPostlist/project.tsx
+++ src/pages/tagPostlist/project.tsx
@@ -281,7 +281,7 @@ const CardDiv = styled.div`
     margin-top: 15px;
     padding: 20px;
     width: 300px;
-    width: 100%;
+
     box-shadow: rgba(231, 211, 255, 10) 0px 1px 2px 0px,
         rgba(231, 211, 255, 0.5) 0px 2px 6px 2px;
     h1 {
